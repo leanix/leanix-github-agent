ca948b281555efcf2d0274ca5f07b056fad64c4e
mohamedlajmileanix <amin.lajmi@leanix.net>
2024-11-15 18:50:49 +0100

    CID-3218: Agent should re-try reconnecting to back if connection closed

--- src/main/kotlin/net/leanix/githubagent/handler/BrokerStompSessionHandler.kt
+++ src/main/kotlin/net/leanix/githubagent/handler/BrokerStompSessionHandler.kt
@@ -9,10 +9,18 @@ import org.springframework.messaging.simp.stomp.StompHeaders
 import org.springframework.messaging.simp.stomp.StompSession
 import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter
 import org.springframework.stereotype.Component
+import java.util.Timer
+import java.util.TimerTask
 import java.util.concurrent.CountDownLatch
 
 @Component
 class BrokerStompSessionHandler : StompSessionHandlerAdapter() {
+
+    companion object{
+        private const val RETRY_INTERVAL = 10000L
+        private const val MAX_RETRY_TIME = 600000L
+    }
+
     @Lazy
     @Autowired
     private lateinit var webSocketService: WebSocketService
@@ -47,10 +55,30 @@ class BrokerStompSessionHandler : StompSessionHandlerAdapter() {
             logger.error("Session closed. This could be due to a network error or the server closing the connection.")
             isConnected = false
             logger.info("Reconnecting...")
-            webSocketService.initSession()
+            retryConnection()
         } else {
             if (latch.count != 0L) latch.countDown()
         }
+    }
+
+    private fun retryConnection() {
+        val timer = Timer()
+        val startTime = System.currentTimeMillis()
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+                if (System.currentTimeMillis() - startTime > MAX_RETRY_TIME) {
+                    logger.error("Failed to reconnect after ${MAX_RETRY_TIME / 60000} minutes. Stopping retries.")
+                    timer.cancel()
+                    return
+                }
+                logger.info("Attempting to reconnect...")
+                webSocketService.initSession()
+                if (isConnected) {
+                    logger.info("Reconnected successfully.")
+                    timer.cancel()
+                }
+            }
+        }, 0, RETRY_INTERVAL) // Retry every 10 seconds
     }
 
     fun isConnected(): Boolean {
