28167044a1e4cd009854da1abcbf724d64dc0c1e
mohamedlajmileanix <amin.lajmi@leanix.net>
2025-05-06 09:49:16 +0200

    CID-3792: Simplify implementation

--- src/main/kotlin/net/leanix/githubagent/config/WebSocketClientConfig.kt
+++ src/main/kotlin/net/leanix/githubagent/config/WebSocketClientConfig.kt
@@ -5,7 +5,6 @@ import io.github.resilience4j.retry.annotation.Retry
 import net.leanix.githubagent.handler.BrokerStompSessionHandler
 import net.leanix.githubagent.services.LeanIXAuthService
 import net.leanix.githubagent.shared.GitHubAgentProperties.GITHUB_AGENT_VERSION
-import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.converter.MappingJackson2MessageConverter
@@ -17,7 +16,6 @@ import org.springframework.web.socket.client.standard.StandardWebSocketClient
 import org.springframework.web.socket.messaging.WebSocketStompClient
 import org.springframework.web.socket.sockjs.client.SockJsClient
 import org.springframework.web.socket.sockjs.client.WebSocketTransport
-import java.util.concurrent.ScheduledFuture
 
 @Configuration
 class WebSocketClientConfig(
@@ -27,9 +25,6 @@ class WebSocketClientConfig(
     private val leanIXProperties: LeanIXProperties,
     private val gitHubEnterpriseProperties: GitHubEnterpriseProperties
 ) {
-    private var heartbeatTask: ScheduledFuture<*>? = null
-    private val logger = LoggerFactory.getLogger(WebSocketClientConfig::class.java)
-
     @Retry(name = "ws_init_session")
     fun initSession(): StompSession {
         val headers = WebSocketHttpHeaders()
@@ -37,37 +32,12 @@ class WebSocketClientConfig(
         stompHeaders["Authorization"] = "Bearer ${leanIXAuthService.getBearerToken()}"
         stompHeaders["GitHub-Enterprise-URL"] = gitHubEnterpriseProperties.baseUrl
         stompHeaders["GitHub-Agent-Version"] = GITHUB_AGENT_VERSION
-        val session = stompClient().connectAsync(
+        return stompClient().connectAsync(
             leanIXProperties.wsBaseUrl,
             headers,
             stompHeaders,
             brokerStompSessionHandler,
         ).get()
-
-        sendHeartbeat(session)
-        return session
-    }
-
-    fun sendHeartbeat(session: StompSession) {
-        val scheduler = ThreadPoolTaskScheduler()
-        scheduler.initialize()
-        heartbeatTask = scheduler.scheduleAtFixedRate({
-            kotlin.runCatching {
-                if (session.isConnected) {
-                    session.send("/app/ghe/heartbeat", "")
-                    logger.debug("Heartbeat sent to /app/heartbeat")
-                } else {
-                    logger.warn("Session is not connected, stopping heartbeat")
-                    stopHeartbeat()
-                }
-            }.onFailure {
-                logger.error("Failed to send heartbeat: ${it.message}")
-            }
-        }, java.time.Duration.ofSeconds(10))
-    }
-
-    fun stopHeartbeat() {
-        heartbeatTask?.cancel(true)
     }
 
     @Bean
@@ -80,6 +50,7 @@ class WebSocketClientConfig(
         val sockJsClient = SockJsClient(transports)
         val stompClient = WebSocketStompClient(sockJsClient)
         stompClient.messageConverter = jsonConverter
+        stompClient.defaultHeartbeat = longArrayOf(3000, 3000)
         val scheduler = ThreadPoolTaskScheduler()
         scheduler.initialize()
         stompClient.taskScheduler = scheduler
