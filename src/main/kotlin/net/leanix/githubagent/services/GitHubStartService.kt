cce9e093c0ddb891b6b7b7fc10daab5f936e8fe6
mohamedlajmileanix <amin.lajmi@leanix.net>
2025-06-12 15:53:48 +0200

    CID-3864: Make full scan run only once

--- src/main/kotlin/net/leanix/githubagent/services/GitHubStartService.kt
+++ src/main/kotlin/net/leanix/githubagent/services/GitHubStartService.kt
@@ -1,7 +1,6 @@
 package net.leanix.githubagent.services
 
 import net.leanix.githubagent.dto.ConnectionEstablishedEvent
-import net.leanix.githubagent.exceptions.UnableToSendMessageException
 import net.leanix.githubagent.shared.APP_NAME_TOPIC
 import org.springframework.context.event.EventListener
 import org.springframework.scheduling.annotation.Async
@@ -11,44 +10,23 @@ import org.springframework.stereotype.Service
 class GitHubStartService(
     private val githubAuthenticationService: GitHubAuthenticationService,
     private val webSocketService: WebSocketService,
-    private val gitHubScanningService: GitHubScanningService,
     private val gitHubEnterpriseService: GitHubEnterpriseService,
     private val cachingService: CachingService,
-    private val syncLogService: SyncLogService
 ) {
-    companion object {
-        var requireScan: Boolean = true
-    }
 
     @SuppressWarnings("UnusedParameter")
     @Async
     @EventListener
     fun startAgent(connectionEstablishedEvent: ConnectionEstablishedEvent) {
-        verifyAndStartScan()
-    }
-
-    fun verifyAndStartScan() {
-        if (requireScan) {
-            runCatching {
-                requireScan = false
-                syncLogService.sendFullScanStart(null)
-                scanResources()
-            }.onSuccess {
-                syncLogService.sendFullScanSuccess()
-            }.onFailure {
-                if (it is UnableToSendMessageException) requireScan = true
-                syncLogService.sendFullScanFailure(it.message)
-            }
-        }
-    }
-
-    private fun scanResources() {
         githubAuthenticationService.generateAndCacheJwtToken()
+        sendAppName()
+    }
+
+    private fun sendAppName() {
         val jwt = cachingService.get("jwtToken") as String
         webSocketService.sendMessage(
             APP_NAME_TOPIC,
             gitHubEnterpriseService.getGitHubApp(jwt).slug
         )
-        gitHubScanningService.scanGitHubResources()
     }
 }
