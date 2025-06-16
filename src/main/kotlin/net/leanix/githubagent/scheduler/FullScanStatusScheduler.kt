c633c3591b591cbe163660ba4be92b92225aecf2
mohamedlajmileanix <amin.lajmi@leanix.net>
2025-06-17 01:54:53 +0200

    CID-3864: Send FullScanStatus

---
+++ src/main/kotlin/net/leanix/githubagent/scheduler/FullScanStatusScheduler.kt
@@ -0,0 +1,21 @@
+package net.leanix.githubagent.scheduler
+
+import net.leanix.githubagent.dto.FullScanStatus
+import net.leanix.githubagent.services.CachingService
+import net.leanix.githubagent.services.WebSocketService
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class FullScanStatusScheduler(
+    private val webSocketService: WebSocketService,
+    private val cachingService: CachingService,
+) {
+
+    @Scheduled(fixedDelay = 10000, initialDelay = 10000)
+    fun sendFullScanStatus() {
+        val runId = cachingService.get("runId") as String?
+        val status = if (runId != null) FullScanStatus.IN_PROGRESS else FullScanStatus.FINISHED
+        webSocketService.sendMessage("fullScan/status", status.name)
+    }
+}
